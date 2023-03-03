1aa76a75fbed186a348a9980880877ac551f81f9
Kartik Barman <121763970+kartik-barman@users.noreply.github.com>
2023-03-03 23:20:03 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 function DeleteMe () {
-    document.getElementById("myResult").value = "";
-}
-function calculator (e) {
-    document.getElementById('myResult').value += e;
-}
-
-function answer () {
-    var a = document.getElementById('myResult').value ;
-    var b = eval(a);
-    document.getElementById('myResult').value = b ;
-}
+    document.getElementById("myResults").value = "";
+    }
+    function backSpace () {
+        var a = document.getElementById("myResults").value ;
+        document.getElementById("myResults"). value=a. substring(0, a. length -1) ;
+    }
+   function calculator(e) {
+document.getElementById("myResults").value += e;
+    }
+   function answer (){
+    var x = document.getElementById("myResults").value ;
+    var y = eval(x) ;
+    document.getElementById("myResults").value = y ;
+   }
